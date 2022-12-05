45307d833f8fd01cfa4050149fd0af330e8fb468
Daniel <daniel.vasev1982@gmail.com>
2022-12-05 14:35:40 +0000

    New Project Working with numbers is added to the main branch. Add project files.

---
+++ WorkingWithNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Reminder
+using System.Runtime.ConstrainedExecution;
+
+Console.WriteLine(6 % 4);
+
+
+int num = 5;
+
+//Increment with 1 
+num++;
+
+Console.WriteLine(num);
+
+//Absolut Value Math.Abs 
+
+Console.WriteLine(Math.Abs(num));
+
+// Power from some number 
+
+Console.WriteLine(Math.Pow(num, 3));
+
+// Math.sqrt
+
+Console.WriteLine(Math.Sqrt(36));
+
+//Math Max and Min 
+
+Console.WriteLine(Math.Max(32, 333));
+Console.WriteLine(Math.Min(32, 333));
+
+// Math.Round rounding the numbers 
+
+double num2 = 4.45;
+Console.WriteLine(Math.Round(num2));
